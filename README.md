7e12e05e00e14a7d3e91891426899c45ffea9bd9
RohitE25 <kumarrohit.2512003@gmail.com>
2024-09-11 14:10:31 +0530

    every thing completed also console was cleared

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Clock
 using HTML,CSS,JAVASCRIPT
+A clock which done through mostly with css property with also have integrated some logic using javascript.
+
+a Small project for pratice
--- app.js
+++ app.js
@@ -3,23 +3,18 @@ let min = document.querySelector(".minLine");
 let second = document.querySelector(".secendsLine");
 
 function hour1(){
-    console.log("ROhit")
     const now = new Date();
     const seconds = now.getSeconds();
     const secondDegree =((seconds/60)*360)+90;
-    // console.log(secondDegree)
      second.style.transform=`rotate(${secondDegree}deg)`
 
 
     const mins =now.getMinutes()
-    // console.log(mins)
     const minDegree = ((mins/60)*360)+90;
     min.style.transform=`rotate(${minDegree}deg)`
 
     const hourr =now.getHours()
-    console.log(hourr)
     const hourdegree = ((hourr/12)*360)+90;
-    console.log(hourdegree)
     hour.style.transform=`rotate(${hourdegree}deg)`
 }
 setInterval(() => {
